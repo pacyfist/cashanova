973b51f7ba517bc8ec159d7a0abba80869561033
Filip Franik <pacyfist@gmail.com>
2025-03-07 18:32:04 +0100

    DaisyUI setup.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { httpResource } from '@angular/common/http';
+import { Component, computed, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'cashanova';
+
+  table = signal<string>('a');
+
+  tableResource = httpResource<Table[]>(
+    () => `https://api.nbp.pl/api/exchangerates/tables/${this.table()}/`,
+  );
+
+  rates = computed(() => this.tableResource.value()?.[0].rates);
+}
+
+export interface Rate {
+  currency: string;
+  code: string;
+  mid: number;
+}
+
+export interface Table {
+  table: string;
+  no: string;
+  effectiveDate: string;
+  rates: Rate[];
 }
