457bdd8596126d3ad9d637c0660c20b9371cdb87
Filip Franik <filip.franik@dualog.com>
2025-03-21 20:32:10 +0100

    Added "font awesome" support

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,18 @@ import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TablesService } from './nbp/services/tables.service';
 import { TitleCasePipe } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import * as icons from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TitleCasePipe],
+  imports: [RouterOutlet, TitleCasePipe, FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'cashanova';
+  icons = icons;
 
   tables = inject(TablesService);
   rates = this.tables.rates;
